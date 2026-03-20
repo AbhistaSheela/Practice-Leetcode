f514ac873e8ac3d54ef2d321ad171758b889f56e
Lakshmi Abhista Sheela <abhista.sheela@gmail.com>
2026-03-20 10:25:30 +0530

    Time: 485 ms (77.17%), Space: 103.8 MB (82.61%) - LeetHub

--- 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.py
+++ 3212-count-submatrices-with-equal-frequency-of-x-and-y/3212-count-submatrices-with-equal-frequency-of-x-and-y.py
@@ -1,24 +1,4 @@
 class Solution:
-    def numberOfSubmatrices(self, grid: List[List[str]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        sumX = [0] * cols
-        sumY = [0] * cols
-        res = 0
-
-        for i in range(rows):
-            rx = 0
-            ry = 0
-            for j in range(cols):
-                if grid[i][j] == 'X':
-                    rx += 1
-                elif grid[i][j] == 'Y':
-                    ry += 1
-                
-                sumX[j] += rx
-                sumY[j] += ry
-                
-                if sumX[j] > 0 and sumX[j] == sumY[j]:
-                    res += 1
-
-        return res
+    def numberOfSubmatrices(self, g: List[List[str]]) -> int:
+        p = (accumulate(map({'X':1,'Y':1j,'.':0}.get,r)) for r in g)
+        return sum(0<q.real==q.imag for c in zip(*p) for q in accumulate(c))
